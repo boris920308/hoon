99b4b8cc55cb759c6f9334bd6e99d2622af2943a
boris920308 <jhoonj0308@gmail.com>
2023-03-03 17:35:21 +0900

    # CodeLab 0. 작업내용 저장 1. unit2/part3/04

--- CodeLab/AndroidBasicsInKotlin/Unit2/part3/03 Affirmations/app/src/main/java/codelab/hoon/affirmations/adapter/ItemAdapter.kt
+++ CodeLab/AndroidBasicsInKotlin/Unit2/part3/03 Affirmations/app/src/main/java/codelab/hoon/affirmations/adapter/ItemAdapter.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import codelab.hoon.affirmations.R
@@ -23,14 +24,15 @@ class ItemAdapter(
 
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
         val item = dataset[position]
-        holder.textView.text =  context.resources.getString(item.stringResourceId)
+        holder.textView.text = context.resources.getString(item.stringResourceId)
+        holder.imageView.setImageResource(item.imageResourceId)
     }
 
     override fun getItemCount() = dataset.size
 
 
-    class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
+    class ItemViewHolder(private val view: View): RecyclerView.ViewHolder(view) {
         val textView: TextView = view.findViewById(R.id.item_title)
-
+        val imageView: ImageView = view.findViewById(R.id.item_image)
     }
 }
