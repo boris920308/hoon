1aa6eb9ee3f491ff9e8005eb8dd8c0854f18272b
boris920308 <jhoonj0308@gmail.com>
2023-03-28 18:11:49 +0900

    # CodeLab [feat] 0.unit3/pathway04/02 1. 가격 계산 로직 추가

--- CodeLab/AndroidBasicsInKotlin/Unit3/PATHWAY04/02/android-basics-kotlin-cupcake-app-starter/app/src/main/java/com/example/cupcake/model/OrderViewModel.kt
+++ CodeLab/AndroidBasicsInKotlin/Unit3/PATHWAY04/02/android-basics-kotlin-cupcake-app-starter/app/src/main/java/com/example/cupcake/model/OrderViewModel.kt
@@ -6,6 +6,9 @@ import androidx.lifecycle.ViewModel
 import java.text.SimpleDateFormat
 import java.util.*
 
+private const val PRICE_PER_CUPCAKE = 2.00
+private const val PRICE_FOR_SAME_DAY_PICKUP = 3.00
+
 class OrderViewModel : ViewModel() {
 
     private val _quantity = MutableLiveData<Int>()
@@ -24,6 +27,7 @@ class OrderViewModel : ViewModel() {
 
     fun setQuantity(numberCupcakes: Int) {
         _quantity.value = numberCupcakes
+        updatePrice()
     }
 
     fun setFlavor(desiredFlavor: String) {
@@ -32,6 +36,7 @@ class OrderViewModel : ViewModel() {
 
     fun setDate(pickupDate: String) {
         _date.value = pickupDate
+        updatePrice()
     }
 
     fun hasNoFlavorSet(): Boolean {
@@ -58,6 +63,14 @@ class OrderViewModel : ViewModel() {
         _flavor.value = ""
         _date.value = dateOptions[0]
         _price.value = 0.0
+    }
+
+    private fun updatePrice() {
+        var calculatedPrice = (quantity.value ?: 0) * PRICE_PER_CUPCAKE
+        if (dateOptions[0] == _date.value) {
+            calculatedPrice += PRICE_FOR_SAME_DAY_PICKUP
+        }
+        _price.value = calculatedPrice
     }
 
 }
