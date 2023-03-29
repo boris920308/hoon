971122320d56dd4fb10f0b6b80ef4cfe3f45f9c8
boris920308 <jhoonj0308@gmail.com>
2023-03-29 18:09:13 +0900

    # CodeLab [feat] 0.unit3/pathway04/03 1. 백스택 관리추가

--- CodeLab/AndroidBasicsInKotlin/Unit3/PATHWAY04/02~03/android-basics-kotlin-cupcake-app-starter/app/src/main/java/com/example/cupcake/FlavorFragment.kt
+++ CodeLab/AndroidBasicsInKotlin/Unit3/PATHWAY04/02~03/android-basics-kotlin-cupcake-app-starter/app/src/main/java/com/example/cupcake/FlavorFragment.kt
@@ -72,4 +72,9 @@ class FlavorFragment : Fragment() {
         super.onDestroyView()
         binding = null
     }
+
+    fun cancelOrder() {
+        sharedViewModel.resetOrder()
+        findNavController().navigate(R.id.action_flavorFragment_to_startFragment)
+    }
 }
