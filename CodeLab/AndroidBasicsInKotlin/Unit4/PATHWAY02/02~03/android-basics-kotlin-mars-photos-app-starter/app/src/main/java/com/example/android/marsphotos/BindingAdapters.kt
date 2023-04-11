007966f364f37dfa773f994669eaadabc82ff49b
boris920308 <jhoonj0308@gmail.com>
2023-04-11 14:25:19 +0900

    # CodeLab [ fix ] 0.unit4/pathway04/03 1. binding adapter수정

--- CodeLab/AndroidBasicsInKotlin/Unit4/PATHWAY02/02~03/android-basics-kotlin-mars-photos-app-starter/app/src/main/java/com/example/android/marsphotos/BindingAdapters.kt
+++ CodeLab/AndroidBasicsInKotlin/Unit4/PATHWAY02/02~03/android-basics-kotlin-mars-photos-app-starter/app/src/main/java/com/example/android/marsphotos/BindingAdapters.kt
@@ -3,18 +3,20 @@ package com.example.android.marsphotos
 import android.widget.ImageView
 import androidx.core.net.toUri
 import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.RecyclerView
 import coil.load
+import com.example.android.marsphotos.network.MarsPhoto
+import com.example.android.marsphotos.overview.PhotoGridAdapter
 
-class BindingAdapters {
-
-    @BindingAdapter("imageUrl")
-    fun bindImage(imageView: ImageView, imageUrl: String?) {
-        imageUrl?.let {
-            val imageUri = imageUrl.toUri().buildUpon().scheme("https").build()
-            imageView.load(imageUri) {
-                placeholder(R.drawable.loading_animation)
-                error(R.drawable.ic_broken_image)
-            }
+@BindingAdapter("imageUrl")
+fun bindImage(imageView: ImageView, imageUrl: String?) {
+    imageUrl?.let {
+        val imageUri = imageUrl.toUri().buildUpon().scheme("https").build()
+        imageView.load(imageUri) {
+            placeholder(R.drawable.loading_animation)
+            error(R.drawable.ic_broken_image)
         }
     }
+}
+
 }
