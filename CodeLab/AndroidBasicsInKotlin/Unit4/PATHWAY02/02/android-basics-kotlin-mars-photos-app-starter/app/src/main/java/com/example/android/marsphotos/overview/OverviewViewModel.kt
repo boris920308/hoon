25051108f9d7ce26ead4e4bf322ff4b9f3243966
boris920308 <jhoonj0308@gmail.com>
2023-04-11 10:23:53 +0900

    # CodeLab [feat][coroutine] 0.unit4/pathway02/02 1. 인터넷이 연결되지 않을경우 예외처리 추가

--- CodeLab/AndroidBasicsInKotlin/Unit4/PATHWAY02/02/android-basics-kotlin-mars-photos-app-starter/app/src/main/java/com/example/android/marsphotos/overview/OverviewViewModel.kt
+++ CodeLab/AndroidBasicsInKotlin/Unit4/PATHWAY02/02/android-basics-kotlin-mars-photos-app-starter/app/src/main/java/com/example/android/marsphotos/overview/OverviewViewModel.kt
@@ -51,8 +51,12 @@ class OverviewViewModel : ViewModel() {
      */
     private fun getMarsPhotos() {
         viewModelScope.launch {
-            val listResult = MarsApi.retrofitService.getPhotos()
-            _status.value = listResult
+            try {
+                val listResult = MarsApi.retrofitService.getPhotos()
+                _status.value = listResult
+            } catch (e: Exception) {
+                _status.value = "Failure: ${e.message}"
+            }
         }
     }
 }
