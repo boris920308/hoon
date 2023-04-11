e60394e8669d113c09f2a792725f95654e038853
boris920308 <jhoonj0308@gmail.com>
2023-04-11 15:23:29 +0900

    # CodeLab [ feat ] 0.unit4/pathway02/04 1. debug code

--- CodeLab/AndroidBasicsInKotlin/Unit4/PATHWAY02/04/Debugging/app/src/main/java/codelab/hoon/debugging/MainActivity.kt
+++ CodeLab/AndroidBasicsInKotlin/Unit4/PATHWAY02/04/Debugging/app/src/main/java/codelab/hoon/debugging/MainActivity.kt
@@ -2,10 +2,24 @@ package codelab.hoon.debugging
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+
+public val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        division()
+    }
+
+    fun division() {
+        val numerator = 60
+        var denominator = 4
+        repeat(4) {
+            Log.v(TAG, "${numerator / denominator}")
+            denominator--
+        }
+
     }
 }
