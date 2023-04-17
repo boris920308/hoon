9edd7465ac9d8736d23b131f0c130e2882b8caa1
boris920308 <jhoonj0308@gmail.com>
2023-04-17 11:46:41 +0900

    # CodeLab [ feat ] 0.unit5/pathway01/02 1. Entity, Dao, ViewModel 정의

---
+++ CodeLab/AndroidBasicsInKotlin/Unit5/PATHWAY01/02/android-basics-kotlin-bus-schedule-app-starter/app/src/main/java/com/example/busschedule/viewmodels/BusScheduleViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.busschedule.viewmodels
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.busschedule.database.schedule.Schedule
+import com.example.busschedule.database.schedule.ScheduleDao
+
+class BusScheduleViewModel(private val scheduleDao: ScheduleDao): ViewModel() {
+
+    fun fullSchedule(): List<Schedule> = scheduleDao.getAll()
+
+    fun scheduleForStopName(name: String): List<Schedule> = scheduleDao.getByStopName(name)
+}
