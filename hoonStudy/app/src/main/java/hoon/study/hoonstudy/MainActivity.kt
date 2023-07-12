020e1dbd7ff6b985317dd9659ae687d1ceb85456
1thefullHoonjae <jhoonj0308@gmail.com>
2023-07-13 08:52:21 +0900

    [feat] viewbinding

--- hoonStudy/app/src/main/java/hoon/study/hoonstudy/MainActivity.kt
+++ hoonStudy/app/src/main/java/hoon/study/hoonstudy/MainActivity.kt
@@ -2,10 +2,18 @@ package hoon.study.hoonstudy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import hoon.study.hoonstudy.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.tvMain.text = "hello view binding"
     }
 }
