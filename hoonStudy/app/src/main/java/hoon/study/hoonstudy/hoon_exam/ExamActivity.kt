c1daf2f8feb2160024f1354591881abbb49aeef8
boris920308 <jhoonj0308@gmail.com>
2023-07-17 10:06:14 +0900

    [feat] ExamActivity validation 구현

--- hoonStudy/app/src/main/java/hoon/study/hoonstudy/hoon_exam/ExamActivity.kt
+++ hoonStudy/app/src/main/java/hoon/study/hoonstudy/hoon_exam/ExamActivity.kt
@@ -3,13 +3,10 @@ package hoon.study.hoonstudy.hoon_exam
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
-import android.util.Log
-import androidx.core.widget.addTextChangedListener
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import hoon.study.hoonstudy.BaseActivity
 import hoon.study.hoonstudy.databinding.ActivityExamBinding
-import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 
 
@@ -22,19 +19,30 @@ class ExamActivity : BaseActivity<ActivityExamBinding>({ ActivityExamBinding.inf
 
         examViewModel = ViewModelProvider(this).get(ExamViewModel::class.java)
 
-
         binding.tv1.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
             override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
             override fun afterTextChanged(p0: Editable?) {
-                examViewModel.tempCounting(p0.toString().length)
+                lifecycleScope.launch {
+                    examViewModel.vali_1(p0.toString().length)
+                    examViewModel.validationText()
+                }
             }
         })
 
+        binding.tv2.addTextChangedListener(object : TextWatcher {
+                override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+                override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+                override fun afterTextChanged(p0: Editable?) {
+                    examViewModel.vali_2(p0.toString().length)
+                    examViewModel.validationText()
+                }
+            }
+        )
+
         lifecycleScope.launch {
             launch {
-                examViewModel.loginBtnEvent.collect() {
-                    Log.d("ExamActivity", "loginBtnEvent is $it")
+                examViewModel.validation1.collect() {
                     binding.btnLogin.isEnabled = it
                 }
             }
--- hoonStudy/app/src/main/java/hoon/study/hoonstudy/hoon_exam/ExamViewModel.kt
+++ hoonStudy/app/src/main/java/hoon/study/hoonstudy/hoon_exam/ExamViewModel.kt
@@ -1,6 +1,5 @@
 package hoon.study.hoonstudy.hoon_exam
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.MutableSharedFlow
@@ -9,29 +8,24 @@ import kotlinx.coroutines.launch
 
 class ExamViewModel : ViewModel() {
 
-    private val _validation1 = MutableSharedFlow<String>()
+    private val _validation1 = MutableSharedFlow<Boolean>()
     val validation1 = _validation1.asSharedFlow()
-    private val _validation2 = MutableSharedFlow<String>()
-    val validation2 = _validation2.asSharedFlow()
+    private var tv1Length: Int = 0
+    private var tv2Length: Int = 0
 
-    private val _loginBtnEvent = MutableSharedFlow<Boolean>()
-    val loginBtnEvent = _loginBtnEvent.asSharedFlow()
-
-
-//    private val _loginBtnEvent = MutableSharedFlow<Boolean>()
-//    val loginBtnEvent = _loginBtnEvent.asSharedFlow()
-
-//    fun isLoginEnabled() {
-//        viewModelScope.launch {
-//
-//        }
-//    }
-
-    fun tempCounting(length: Int) {
+    fun validationText() {
         viewModelScope.launch {
-            Log.d("ExamViewModel", "tempCounting_1, length = $length")
-            _loginBtnEvent.emit(length > 5)
+            _validation1.emit(tv1Length > 3 && tv2Length > 5)
         }
     }
+
+    fun vali_1(length: Int) {
+        tv1Length = length
+    }
+
+    fun vali_2(length: Int) {
+        tv2Length = length
+    }
+
 
 }
